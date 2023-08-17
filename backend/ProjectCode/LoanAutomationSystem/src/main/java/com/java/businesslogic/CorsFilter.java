fb42e835a8f27ca192722e3b1f35a25390a1b0f8
rukmanandareddy02 <rukmanandareddyabbidi@gmail.com>
2023-08-17 10:39:56 +0530

    updated cors filter

--- backend/ProjectCode/LoanAutomationSystem/src/main/java/com/java/businesslogic/CorsFilter.java
+++ backend/ProjectCode/LoanAutomationSystem/src/main/java/com/java/businesslogic/CorsFilter.java
@@ -5,21 +5,17 @@ import java.io.IOException;
 import jakarta.ws.rs.container.ContainerRequestContext;
 import jakarta.ws.rs.container.ContainerResponseContext;
 import jakarta.ws.rs.container.ContainerResponseFilter;
-
-public class CorsFilter implements ContainerResponseFilter{
+import jakarta.ws.rs.ext.Provider;
+@Provider
+public class CorsFilter implements ContainerResponseFilter {
 	@Override
-	public void filter(ContainerRequestContext requestContext,ContainerResponseContext responseContext) throws IOException {
-		//requestContext.getHeaders().add(arg0, arg1);
-		 responseContext.getHeaders().add(
-		            "Access-Control-Allow-Origin", "*");
-		          responseContext.getHeaders().add(
-		            "Access-Control-Allow-Credentials", "true");
-		          responseContext.getHeaders().add(
-		           "Access-Control-Allow-Headers",
-		           "origin, content-type, accept, authorization");
-		          responseContext.getHeaders().add(
-		            "Access-Control-Allow-Methods",
-		            "GET, POST, PUT, DELETE, OPTIONS, HEAD");
-		
+	public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext)
+			throws IOException {
+		// requestContext.getHeaders().add(arg0, arg1);
+		responseContext.getHeaders().add("Access-Control-Allow-Origin", "*");
+		responseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
+		responseContext.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
+		responseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+
 	}
 }
